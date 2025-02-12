89e9c31900746a051f1f873cd7786c1e8b049cbe
baranovr605 <baranovr604@gmail.com>
2025-02-13 02:54:34 +0300

    Fix for pep8

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,9 +1,10 @@
-from cats.views import AchievementViewSet, CatViewSet
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+
+from cats.views import AchievementViewSet, CatViewSet
 
 router = routers.DefaultRouter()
 router.register(r"cats", CatViewSet)
@@ -21,3 +22,4 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
+
